83033828cc9c9d15886798978a5a7365b6c118a6
matteocanale12 <matteocanale12@gmail.com>
2020-11-24 20:35:02 +0100

    Aggiunto MOST POP songs and artist

--- Spotify_Analysis.R
+++ Spotify_Analysis.R
@@ -81,3 +81,22 @@ for (i in 2:length(mean_master)) {
   plot(mean_master[, 1], mean_master[, i], type="l", col="blue", lwd=3, xlab="year", ylab=names(mean_master)[i], main=paste("100 yrs of", names(mean_master)[i]))
 }
 
+#################################################
+
+# first we use the mean, we notice in the output one-time hit artist
+# artist who recently got a hit (how recent is weighted on the 'populatity' variable) a not many more song (low denominator).
+pop_artist = aggregate(raw_data$popularity, list(raw_data$artists), mean)
+sort_pa = pop_artist[order(pop_artist[, 2],decreasing=TRUE),]
+head(sort_pa)
+
+# using the sum, we encounter in the output long time established artist with several evergreen
+# each one with high values of 'populariry'
+pop_artist = aggregate(raw_data$popularity, list(raw_data$artists), sum)
+sort_pa = pop_artist[order(pop_artist[, 2],decreasing=TRUE),]
+head(sort_pa)
+
+# the most popular songs
+pop_songs = raw_data[order(raw_data[, "popularity"], decreasing=TRUE),]
+pop_songs[1:3, "name"]
+
+
