18c74fd49021efdfe88ecb63e94577848794e938
Gabriele Sartor <gabrielesartor4@gmail.com>
2020-12-06 13:23:27 +0100

    release with plots

--- Sources/1_spotify_univariate_analysis.R
+++ Sources/1_spotify_univariate_analysis.R
@@ -3,6 +3,9 @@ library(ggpubr)
 library(farver)
 library(moments)
 library(dplyr)  
+library(ggplot2)
+library(stringr) 
+require(plyr)
 
 
 ###################################
@@ -32,7 +35,10 @@ clean_data <- subset(data, data$duration_s > (q1 - 1.5*iqr) & data$duration_s < 
 ### END DATASET CLEANING ###
 ############################
 
-
+rm(iqr)
+rm(q1)
+rm(q2)
+rm(q3)
 
 ## Numerical subset
 
@@ -42,7 +48,9 @@ numerical_columns_names <- c("popularity", "instrumentalness", "acousticness", "
 
 # Shows distribution for each numerical column
 for (attribute in numerical_columns_names) {
-  hist(clean_data[attribute][,], main=paste("Histogram ", attribute), xlab=paste(attribute))
+  print(ggplot(clean_data, aes(x=clean_data[attribute][,])) + 
+    geom_histogram(aes(y=..density..), colour="black", fill="#1DB954")+
+    labs(title=str_to_title(paste(gsub("_", " ",attribute), "histogram plot")),x=attribute, y = "Density"));
 }
 
 
@@ -50,37 +58,45 @@ for (attribute in numerical_columns_names) {
 head(clean_data)
 
 #Create x different time periods with labels
-time_periods <- list(  c(1920,1949), 
-                       c(1950,1979), 
-                       c(1980,1999), 
-                       c(2000,2009), 
-                       c(2010,2020))
-time_periods_labels <- list( "20s-40s", "50s-70s", "80s-90s", "00s", "10s" )
+time_periods <- c(  1920,1950,1980, 2000, 2010)
+time_periods_labels <- c( "20s-40s", "50s-70s", "80s-90s", "00s", "10s" )
+
+clean_data$time_period <- mapvalues(clean_data$year, 
+                               from=seq(1921,2020), 
+                               to=c(replicate(length(seq(1921,1949)), 1920), replicate(length(seq(1950,1979)), 1950),
+                                    replicate(length(seq(1980,1999)), 1980), replicate(length(seq(2000,2009)), 2000),
+                                    replicate(length(seq(2010,2020)), 2010)) )
+
+clean_data$time_period <- as.factor(clean_data$time_period)
+
 
 # Compute boxplot, skewness and kurtosis for each column extracted
 for (attribute in numerical_columns_names) {
   print(paste("Computing boxplots, skewness and kurtosis for ", attribute))
-  bp_list = list()
   sk_list = list()
   ku_list = list()
   
+  p <- ggplot(clean_data, aes(time_period, clean_data[attribute][,]))
+  print(p + geom_boxplot(colour="black", fill="#1DB954",
+                         # custom outliers
+                         outlier.colour="black",
+                         outlier.fill="black",
+                         outlier.size=1,
+                         outlier.alpha = 0.3) +
+          labs(title=str_to_title(paste(gsub("_", " ",attribute), "boxplot")),
+               x="Time Period", 
+               y = str_to_title(attribute) ) )
+
   # Compute boxplot, skewness and kurtosis for the selected column for each time period
-  for (i in 1:length(time_periods)) {
-    d <- clean_data[ which(clean_data$year>=time_periods[[i]][1] & clean_data$year <= time_periods[[i]][2]), ]
-    bp_list[[i]] = as.grob(~boxplot(d[attribute],
-                                    horizontal=FALSE, 
-                                    main=paste(time_periods_labels[[i]]),
-                                    #ylim=c(0, 1000 )))
-                                    ylim=c(min(clean_data[attribute]), max(clean_data[attribute]))))
+  for (i in seq(1,length(time_periods))) {
+    d <- clean_data[ which(clean_data$time_period==time_periods[i]), ]
     sk_list[[i]] = skewness(d[attribute])
     ku_list[[i]] = kurtosis(d[attribute])
   }
-  print(ggarrange(plotlist = bp_list, nrow = 1))
+  
   print('Skewness values: ')
   print(sk_list)
   
   print('Kurtosis values: ')
   print(ku_list)
-  
-  readline(prompt="Press [enter] to continue")
 }
--- Sources/2_spotify_multi_analysis.R
+++ Sources/2_spotify_multi_analysis.R
@@ -1,18 +1,55 @@
-#################################################
-# DATASETS NAVIGATION:
-#
-# - clean_data: contains all 20 variables, all types, outlier cleaned
-# {SUBSET OF clean_data}
-#   - dataset: numerical values ('year' maintained for grouping purposes, even if categorical)
-# {SUBSET OF dataset}
-#     - n_dataset: numerical values (popularity included)     --> .sd --> n_dataset.sd (scaled version)
-#     - n_dataset_NP: numerical values (without popularity)   --> .sd --> n_dataset_NP.sd (scaled version)
+###################################
+######## Statistics project #######
+## 100 Years of Music on Spotify ##
+##### Part 2 - Multivariate analysis #####
+###################################
 
-#################################################
+setwd(".")
 
-dataset = clean_data[c('year', "acousticness", "danceability", "duration_s", "energy", 'instrumentalness', 'popularity', "liveness", "loudness", "speechiness", "tempo", "valence")]
-n_dataset = clean_data[c("acousticness", "danceability", "duration_s", "energy", 'instrumentalness', 'popularity', "liveness", "loudness", "speechiness", "tempo", "valence")]
+########################
+### Dataset cleaning ###
+########################
+
+# Do not run if 0_dataset_cleaning has already been executed
+data <- read.csv('../data/spotify_data.csv')
+data$duration_s <- round((data$duration_ms/1000), 1)
+
+q1 = quantile(data$duration_s, 0.25)
+q2 = quantile(data$duration_s, 0.5) # median
+q3 = quantile(data$duration_s, 0.75)
+
+iqr = IQR(data$duration_s)   # or q3 - q1 
+
+clean_data <- subset(data, data$duration_s > (q1 - 1.5*iqr) & data$duration_s < (q2+6*iqr))
+
+### END DATASET CLEANING ###
+############################
+
 n_dataset_NP = clean_data[c("acousticness", "danceability", "energy",  "liveness", "loudness", "speechiness", "tempo", "valence")]
+
+# Multivariate Analysis
+
+#Correlation matrix
+mcor<-cor(n_dataset_NP,use="na.or.complete")
+mcor[upper.tri(mcor)]<-""
+cor_matrix2<-as.data.frame(mcor)
+plot(cor_matrix2)
+
+# correlation table vith values
+library(corrplot)
+cor(n_dataset_NP)
+corrplot(cor(n_dataset_NP), method="number", type="upper")
+corrplot(cor(n_dataset_NP), method="circle", type="upper")
+
+
+#####
+#PCA#
+#####
+## COLORS:
+# Spotify green #1DB954
+# Petrol #398488
+# Red #BE1111
+# Dark grey #313131
 
 library(FactoMineR)
 library(factoextra)
@@ -20,68 +57,27 @@ library(factoextra)
 pc<-prcomp(scale(n_dataset_NP)) #PCA
 summary(pc)
 pc$rotation
-fviz_eig(pc)
+#Components and variability explained
+#TO update graphics
+fviz_eig(pc, barfill = "#1DB954",
+         barcolor = "#1DB954",
+         linecolor = "black")
 
-#par("mar")
-#par(mar=c(2.1,2.1,2.1,2.1))
+#Nice graphic for PC1 and PC2
 fviz_pca_var(pc,
              col.var = "contrib", # Color by contributions to the PC
              gradient.cols = c("#00AFBB", "#E7B800", "#FC4E07"),
              repel = TRUE     # Avoid text overlapping
 )
 
-
-
-pca2_var<-colnames(n_dataset)
+pca2_var<-colnames(n_dataset_NP)
 
 pca2<-PCA(scale(n_dataset_NP),graph=F)
 plot(pca2,choix="var")
 summary(pca2)
 
-cluster2<-HCPC(pca2,nb.clust=4,graph=F)
-plot(cluster2,choice="tree")
-plot(cluster2,choice="map")
-
-#CA
-ca_data<-table(data$sesso_età, data$fumatore) #contingency table
-ca<-CA(ca_data,graph=F)
-plot(ca,choice="ind")
-
-numerical_columns_names <- c("acousticness", "danceability", "energy", "liveness", "loudness", "speechiness", "tempo", "valence")
-
-pca2_var<-colnames(data)[c(6,20:31)]
-
-pca2<-PCA(clean_data[,numerical_columns_names],graph=F)
-plot(pca2,choix="var")
-
-cluster2<-HCPC(pca2,nb.clust=4,graph=F)
-plot(cluster2,choice="tree")
-plot(cluster2,choice="map")
-
-#Correlation matrix
-mcor<-cor(clean_data[numerical_columns_names],use="na.or.complete")
-mcor[upper.tri(mcor)]<-""
-cor_matrix2<-as.data.frame(mcor)
-plot(cor_matrix2)
-
-# Multivariate Analysis
-
-# variance and covariance
-round(var(n_dataset), 2); View(round(var(n_dataset), 2))
-round(var(scale(n_dataset)), 2); View(round(var(scale(n_dataset)), 2))
-round(cor(n_dataset_NP), 2); View(round(cor(n_dataset_NP), 2))
-
-round(var(n_dataset_NP), 2); View(round(var(n_dataset_NP), 2))
-round(var(scale(n_dataset_NP)), 2); View(round(var(scale(n_dataset_NP)), 2))
-round(cor(n_dataset_NP), 2); View(round(cor(n_dataset_NP), 2))
-
-# correlation table vith values
-library(corrplot)
-cor(n_dataset)
-corrplot(cor(n_dataset), method="number", type="upper")
-
-# PCA
-n_dataset.sd <- scale(n_dataset)
+# PCA GRAPHIC 2
+n_dataset.sd <- scale(n_dataset_NP)
 n_dataset.sd <- data.frame(n_dataset.sd)
 
 pc.n_dataset <- princomp(n_dataset.sd, scores=T)
@@ -89,14 +85,16 @@ pc.n_dataset
 summary(pc.n_dataset)
 
 layout(matrix(c(2,3,1,3),2,byrow=T))
-plot(pc.n_dataset, las=2, main='Principal components', ylim=c(0,6))
+plot(pc.n_dataset, las=2, main='Principal components', ylim=c(0,4))
 abline(h=1, col='blue')
-barplot(sapply(n_dataset.sd,sd)^2, las=2, main='Original variables', ylim=c(0,6), ylab='Variances')
-plot(cumsum(pc.n_dataset$sde^2)/sum(pc.n_dataset$sde^2), type='b', axes=F, xlab='Number of components', ylab='Contribution to the total variance', ylim=c(0,1))
+barplot(sapply(n_dataset.sd,sd)^2, las=2, main='Original variables', ylim=c(0,2), ylab='Variances')
+plot(cumsum(pc.n_dataset$sde^2)/sum(pc.n_dataset$sde^2), 
+     type='b', 
+     axes=F, 
+     xlab='Number of components', 
+     ylab='Contribution to the total variance', 
+     ylim=c(0,1))
 box()
 axis(2,at=0:10/10,labels=0:10/10)
 axis(1,at=1:ncol(n_dataset.sd),labels=1:ncol(n_dataset.sd),las=2)
-
-
-# Cumulative Proportion  0.3117392 0.4581311 0.5639063 0.6582767 0.73789660 0.81577504 0.88034955 0.92994783 0.96287794 0.98886493
 
